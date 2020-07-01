31a5d74e8b77ce61e534fb3b9303003c19329511
sieroniekuggy <sieroniekuggy@gmail.com>
2020-07-01 14:13:23 +0800

    Pizza house image added

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Pizza House</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="/css/main.css" rel="stylesheet">
+    </head>
+    <body>
+
+      @yield('content')
+
+      <footer>
+        <p>Copyright 2020 Pizza House</p>
+      </footer>
+    </body>
+</html>
---
+++ resources/views/pizzas.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title m-b-md">
+            Pizza List
+        </div>
+
+        @foreach($pizzas as $pizza)
+          <div>
+            {{ $loop->index }} - {{ $pizza['type'] }} - {{ $pizza['base'] }}
+            @if($loop->first)
+              <span> - first in the loop</span>
+            @endif
+            @if($loop->last)
+              <span> - last in the loop</span>
+            @endif
+          </div>
+        @endforeach
+
+    </div>
+</div>
+@endsection
