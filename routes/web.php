25eb759a6fe646be3c2a38366586ff2d0ecb9695
sieroniekuggy <sieroniekuggy@gmail.com>
2020-07-01 12:45:49 +0800

    Project 1 : Pizzahouse

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\Routing\Router;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/pizzas', function () {
+    //get data from the db
+    $pizzas = [
+        ['type' => 'hawaiian','base' => 'cheesy crust'],
+        ['type' => 'volcano','base' => 'garlic crust'],
+        ['type' => 'veg supreme','base' => 'thin and crispy']
+    ];
+
+    return view('pizzas',[
+        'pizzas' => '$pizzas',
+    ]);
+});
+
+Route::get('/pizzas/{$id}', function ($id) {
+    //use the $id variable to query the db for a record
+    return view('details', ['id' => $id]);
+});
