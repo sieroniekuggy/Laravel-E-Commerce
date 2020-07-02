b09039050d8e41573cbcb0d5d6656bc4115ac26c
sieroniekuggy <sieroniekuggy@gmail.com>
2020-07-02 14:55:12 +0800

    eloquent models

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -3,16 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Pizza;
 
 class PizzaController extends Controller
 {
     public function index() {
         // get data from a database
-        $pizzas = [
-          ['type' => 'hawaiian', 'base' => 'cheesy crust'],
-          ['type' => 'volcano', 'base' => 'garlic crust'],
-          ['type' => 'veg supreme', 'base' => 'thin & crispy']
-        ];
+        // $pizzas = Pizza::all();
+        // $pizzas = Pizza::orderBy('name', 'desc')->get();
+        // $pizzas = Pizza::where('type', 'volcano')->get();
+        $pizzas = Pizza::latest()->get();
 
         return view('pizzas', [
           'pizzas' => $pizzas,
