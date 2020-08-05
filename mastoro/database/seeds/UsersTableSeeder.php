83820b94097b9e18b53090914c2c1b752c5a7324
sieroniekuggy <sieroniekuggy@gmail.com>
2020-08-05 23:35:34 +0800

    Creating Shops

--- mastoro/database/seeds/UsersTableSeeder.php
+++ mastoro/database/seeds/UsersTableSeeder.php
@@ -12,33 +12,33 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('users')->delete();
-        
+
         \DB::table('users')->insert(array (
-            0 => 
+            0 =>
             array (
                 'id' => 1,
-                'role_id' => NULL,
+                'role_id' => 2,
                 'name' => 'Silence Ronald Kugotsi',
                 'email' => 'sieroniekuggy@gmail.com',
                 'avatar' => 'users/default.png',
-                'email_verified_at' => NULL,
+                'email_verified_at' => '2020-08-03 14:17:28',
                 'password' => '$2y$10$CTiIVxvxgK71VUESMqAw9.zj8UDiYFMcZ/VDK27Ulnyu4arb3wekm',
                 'remember_token' => 'c0mlbMSZKSNhrPvvrSVEV9S5iK4ZM2hQXUA5ZB60ToKZD5VE4qMcjtoDCFui',
                 'settings' => NULL,
                 'created_at' => '2020-08-03 14:17:28',
                 'updated_at' => '2020-08-03 14:17:28',
             ),
-            1 => 
+            1 =>
             array (
                 'id' => 3,
                 'role_id' => 1,
                 'name' => 'Admin',
                 'email' => 'admin@admin.com',
                 'avatar' => 'users/default.png',
-                'email_verified_at' => NULL,
+                'email_verified_at' => '2020-08-03 14:17:28',
                 'password' => '$2y$10$R.VPd5TC0QWw.Cr9dd8vD.mlSdK1o/YyYTgQwNKWOSb6K6sL5dqoO',
                 'remember_token' => 'mZLYiKQFS8XuSkCapofC2bAB6JrrmCvQdIOX70rPi7JGESfNo3FqvHAEka4X',
                 'settings' => NULL,
@@ -46,7 +46,7 @@ class UsersTableSeeder extends Seeder
                 'updated_at' => '2020-08-03 22:46:43',
             ),
         ));
-        
-        
+
+
     }
 }
